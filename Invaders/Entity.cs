f53f23f3c308bd61b99cd58ec94f6fc9bec4ee43
Korpo-spec <casper22x@gmail.com>
2021-10-08 10:27:35 +0200

    init commit

---
+++ Invaders/Entity.cs
@@ -0,0 +1,36 @@
+using System;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Invaders
+{
+    public class Entity
+    {
+        private string textureName = "";
+        protected Sprite sprite;
+        public bool Dead;
+
+        public Entity(string textureName)
+        {
+            this.textureName = textureName;
+            sprite = new Sprite();
+        }
+
+        public virtual void Create(Scene scene)
+        {
+            sprite.Texture = scene.Assets.LoadTexture(textureName);
+            
+        }
+
+        public virtual void Destroy(Scene scene)
+        {
+            
+        }
+
+        public Vector2f Position
+        {
+            get => sprite.Position;
+            set => sprite.Position = value;
+        }
+    }
+}
---
+++ Invaders/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SFML.System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Invaders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var window = new RenderWindow(new VideoMode(828, 900), "Pacman")) 
+            {
+                window.Closed += (o, e) => window.Close();
+                
+                
+                
+                Clock clock = new Clock();
+                window.SetView(new View(new FloatRect(18,0,414,450)));
+                while (window.IsOpen)
+                {
+                    
+                    window.DispatchEvents();
+
+                    float deltaTime = clock.Restart().AsSeconds();
+                    if (deltaTime > 0.01f) deltaTime = 0.01f;
+                    
+                    //TODO: UpdateAll
+                    
+                    window.Clear(new Color(223, 246, 245));
+                    // TODO:  Drawing
+                    
+                    window.Display();
+                }
+            }
+        }
+    }
+}
